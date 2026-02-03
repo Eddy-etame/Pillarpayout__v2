a12afe714754978e5a7215c32faa34c6e61426a6
Eddy-etame <eddy.etame@enkoschools.com>
2026-02-03 05:44:45 +0100

    Delete apps directory

--- apps/web/src/app/services/chat.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from '@angular/core';
-
-@Injectable({ providedIn: 'root' })
-export class ChatService {
-  connect() {
-    // Socket.IO integration will be wired in the chat module.
-  }
-}
